72a2be080607e958d66d76efd986f1656b84f5c8
Michael Campbell <michael.campbell14@gmail.com>
2022-08-30 17:12:30 -0400

    project 123

---
+++ project-12-typing-game/scripts.js
@@ -0,0 +1,130 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+// list of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving',
+];
+
+// init word
+let randomWord;
+
+// init score
+let score = 0;
+
+// init time
+let time = 10;
+
+// set difficulty to value in local storage or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// focus the input on start
+text.focus();
+
+// start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// generate random word from array
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+// add word to DOM
+function addWordToDOM() {
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+
+
+// update the score
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+// update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        // end game
+        gameOver();
+    }
+}
+
+// game over 
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+    endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
+
+// event listeners
+text.addEventListener('input', (e) => {
+    const insertedText = e.target.value;
+    
+    if(insertedText === randomWord) {
+        addWordToDOM();
+        updateScore();
+
+        // clear
+        e.target.value = '';
+
+        if(difficulty === 'Master') {
+            time += 2;
+        } else if(difficulty === 'Average') {
+            time += 3;
+        } else {
+            time += 5;
+        }
+
+        updateTime();
+    }
+});
+
+// settings button
+settingsBtn.addEventListener('click', () => {
+    settings.classList.toggle('hide');
+})
+
+// settings select
+settingsForm.addEventListener('change', (e) => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+})
