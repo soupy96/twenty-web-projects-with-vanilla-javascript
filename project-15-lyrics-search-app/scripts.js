1e2e6e36dc4754e491bd606f80ca077ecccaba5a
Michael Campbell <michael.campbell14@gmail.com>
2022-09-02 17:11:25 -0400

    project-15. please read comments in js file

---
+++ project-15-lyrics-search-app/scripts.js
@@ -0,0 +1,101 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// search by term
+async function searchSongs(term) {
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+// show song and artist in DOM
+function showData(data) {
+    // let output = '';
+
+    // data.data.forEach(song => {
+    //     output += `
+    //         <li>
+    //             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+    //             <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    //         </li>
+    //     `;
+    // });
+
+    // result.innerHTML = `
+    //     <ul class="songs">
+    //         ${output}
+    //     </ul>
+    // `;
+
+    result.innerHTML = `
+        <ul class="songs">
+            ${data.data.map(song => `<li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`).join('')}
+        </ul>
+    `;
+
+    if(data.prev || data.next) {
+        more.innerHTML = `
+            ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Previous</button>` : ''}
+            ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+        `;
+    } else {
+        more.innerHTML = '';
+    }
+}
+
+// getting the next or prev page of songs
+async function getMoreSongs(url) {
+    // in order for the heroku cors to work i needed to console.log() the whole url, click on it in the console, then request access for permission to use it
+    // console.log(`https://cors-anywhere.herokuapp.com/${url}`);
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+  
+    showData(data);
+}
+
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    // why every song returns an error https://github.com/NTag/lyrics.ovh/issues/15
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+}
+
+// event listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+    
+    if(!searchTerm) {
+        alert('Please type in a search term.')
+    } else {
+        searchSongs(searchTerm);
+    }
+})
+
+// get lyrics button click
+result.addEventListener('click', (e) => {
+    const clickedEl = e.target;
+
+    if(clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
+    }
+})
